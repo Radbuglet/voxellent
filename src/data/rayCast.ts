213ec2ce2cc61832725df6fff92f91b921729840
Riley Denoyer <10578060+Radbuglet@users.noreply.github.com>
2020-09-04 15:51:59 -0700

    Finish implementing the RayCast

--- src/data/rayCast.ts
+++ src/data/rayCast.ts
@@ -1,18 +1,13 @@
 import {vec3} from "gl-matrix";
 import {Axis, FaceUtils, VoxelFace} from "../utils/faceUtils";
+import {VoxelChunk, VoxelWorld} from "./data";
+import {P$} from "ts-providers";
+import {VoxelPointer} from "./pointer";
 
-export interface IRayCrossEnumerator {
-    // TODO
-}
-
-export class VoxelRayCast {
-    // TODO
-}
-
-export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
+export class VoxelRayCast<TChunk extends P$<typeof VoxelChunk, VoxelChunk<TChunk>>> {
     // >> Status registers
-    public last_breached_face: VoxelFace = VoxelFace.px;
-    public last_distance_traveled: number = 0;
+    public breached_face: VoxelFace = VoxelFace.px;
+    public distance_traveled: number = 0;
 
     // >> Traversal state
     // Represents the ray in world-space. This is used to convert distances back into positions.
@@ -36,22 +31,27 @@ export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
     private dist_at_z_cross!: number;
 
     // >> Construction
-    constructor(origin: vec3, direction: vec3) {
-        this.warpPositionAndDirection(origin, direction);
+    constructor(world: VoxelWorld<TChunk>, origin: vec3, direction: vec3, public readonly pointer: VoxelPointer<TChunk> = new VoxelPointer<TChunk>()) {
+        this.warpPositionAndDirection(world, origin, direction);
     }
 
-    warpPosition(new_origin: vec3) {  // TODO: Implement
+    // >> Warp logic
+    private warpPositionNoPtr(new_origin: vec3) {
         this.origin = new_origin;
-        this.dist_at_x_cross = 0;
-        this.dist_at_y_cross = 0;
-        this.dist_at_z_cross = 0;
+
+        // Get initial distance to cross.
+        this.dist_at_x_cross = this.direction[0] === 0 ? Infinity :  // If this component is zero, we will never cross
+            // Otherwise, the distance is equal to the full length distance times the percent of the axis already traversed.
+            this.cross_dist_step_x * (this.direction[0] > 0 ? -1 : 1) * (Math.floor(this.origin[0]) - this.origin[0]);
+
+        this.dist_at_y_cross = this.direction[1] === 0 ? Infinity :
+            this.cross_dist_step_y * (this.direction[1] > 0 ? -1 : 1) * (Math.floor(this.origin[1]) - this.origin[1]);
+
+        this.dist_at_z_cross = this.direction[2] === 0 ? Infinity :
+            this.cross_dist_step_z * (this.direction[2] > 0 ? -1 : 1) * (Math.floor(this.origin[2]) - this.origin[2]);
     }
 
-    warpDirection(new_direction: vec3) {
-        this.warpPositionAndDirection(this.origin, new_direction);
-    }
-
-    warpPositionAndDirection(new_origin: vec3, new_direction: vec3) {
+    private warpOnlyDirection(new_direction: vec3) {
         // Update the world-space direction state
         this.direction = new_direction;
 
@@ -64,12 +64,26 @@ export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
         this.cross_dist_step_x = Math.abs(1 / new_direction[0]);
         this.cross_dist_step_y = Math.abs(1 / new_direction[1]);
         this.cross_dist_step_z = Math.abs(1 / new_direction[2]);
-
-        // Update the position
-        this.warpPosition(new_origin);
     }
 
-    step() {
+    warpPosition(world: VoxelWorld<any>, new_origin: vec3) {
+        this.pointer.setWorldPos(world, new_origin);  // Pointer needs refresh
+        this.warpPositionNoPtr(new_origin);  // To need to refresh direction.
+    }
+
+    warpDirection(new_direction: vec3) {
+        this.warpOnlyDirection(new_direction);
+        this.warpPositionNoPtr(this.origin);  // The pointer is still valid, no need to refresh.
+    }
+
+    warpPositionAndDirection(world: VoxelWorld<any>, new_origin: vec3, new_direction: vec3) {
+        this.pointer.setWorldPos(world, new_origin);  // Pointer needs refresh
+        this.warpOnlyDirection(new_direction);  // We first update the direction of the vector.
+        this.warpPositionNoPtr(new_origin);  // Then we update the position state to both set the origin and apply it.
+    }
+
+    // >> Ray-cast logic
+    step(world: VoxelWorld<TChunk>) {
         // >> The face we cross will be on the axis which requires the least amount of distance to get to.
         // We will yield the crossed face and then move the dist_at_[x-z]_cross variable to represent the distance for
         // the next cross.
@@ -77,8 +91,11 @@ export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
         // Is x the minimum?
         if (this.dist_at_x_cross < this.dist_at_y_cross && this.dist_at_x_cross < this.dist_at_z_cross) {  // Y cannot be the minimum
             // >> Update registers and yield
-            this.last_breached_face = this.face_x;
-            this.last_distance_traveled = this.dist_at_x_cross;
+            this.breached_face = this.face_x;
+            this.distance_traveled = this.dist_at_x_cross;
+
+            // >> Update pointed voxel
+            this.pointer.getNeighbor(world, this.face_x, this.pointer);
 
             // >> Update next cross distance for axis
             this.dist_at_x_cross += this.cross_dist_step_x;
@@ -88,8 +105,11 @@ export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
         // Is y the minimum?
         if (this.dist_at_y_cross < this.dist_at_z_cross) {
             // >> Update registers and yield
-            this.last_breached_face = this.face_y;
-            this.last_distance_traveled = this.dist_at_y_cross;
+            this.breached_face = this.face_y;
+            this.distance_traveled = this.dist_at_y_cross;
+
+            // >> Update pointed voxel
+            this.pointer.getNeighbor(world, this.face_y, this.pointer);
 
             // >> Update next cross distance for axis
             this.dist_at_y_cross += this.cross_dist_step_y;
@@ -99,19 +119,20 @@ export class FreeRayCrossEnumerator implements IRayCrossEnumerator {
         // Z must be the minimum!
         {
             // >> Update registers and yield
-            this.last_breached_face = this.face_z;
-            this.last_distance_traveled = this.dist_at_z_cross;
+            this.breached_face = this.face_z;
+            this.distance_traveled = this.dist_at_z_cross;
+
+            // >> Update pointed voxel
+            this.pointer.getNeighbor(world, this.face_z, this.pointer);
 
             // >> Update next cross distance for axis
             this.dist_at_z_cross += this.cross_dist_step_z;
         }
     }
 
-    getLastRayFacePosition() {
-        throw "Not implemented"; // TODO
+    getFaceIntersection(target: vec3 = vec3.create()): vec3 {
+        vec3.scale(target, this.direction, this.distance_traveled);
+        vec3.add(target, this.origin, target);
+        return target;
     }
-}
-
-export class AxisCrossEnumerator implements IRayCrossEnumerator {
-    // TODO
 }

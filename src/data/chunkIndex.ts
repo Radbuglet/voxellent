ad94eecb49ef0d4629db1414096d2d678d160005
Riley Denoyer <10578060+Radbuglet@users.noreply.github.com>
2020-10-02 07:22:21 -0700

    Fix bad bitwise usage in ChunkIndex

--- src/data/chunkIndex.ts
+++ src/data/chunkIndex.ts
@@ -4,18 +4,19 @@ import {vec3} from "gl-matrix";
 export type ChunkIndex = number;
 export const ChunkIndex = new class {
     public readonly bits_per_chunk_comp = 4;
-    public readonly chunk_edge_size = 2 ** this.bits_per_chunk_comp;
-    public readonly chunk_voxel_count = this.chunk_edge_size ** 3;
+    public readonly chunk_edge_length = 2 ** this.bits_per_chunk_comp;
+    public readonly chunk_edge_bitmask = this.chunk_edge_length - 1;
+    public readonly chunk_voxel_count = this.chunk_edge_length ** 3;
 
     public register_traversed_chunks = 0;
 
     // Vector-index interactions
     fromWorldVector(vec: Readonly<vec3>) {
-        return this.fromChunkPosition(vec[0] & this.chunk_edge_size, vec[1] & this.chunk_edge_size, vec[2] & this.chunk_edge_size);
+        return this.fromChunkPosition(vec[0] & this.chunk_edge_bitmask, vec[1] & this.chunk_edge_bitmask, vec[2] & this.chunk_edge_bitmask);
     }
 
     fromChunkPosition(x: number, y: number, z: number) {
-        return x + (y << this.bits_per_chunk_comp) + (z << 2 * this.bits_per_chunk_comp);
+        return x + (y << this.bits_per_chunk_comp) + (z << (2 * this.bits_per_chunk_comp));
     }
 
     fromChunkPosVector(vec: Readonly<vec3>) {
@@ -23,11 +24,11 @@ export const ChunkIndex = new class {
     }
 
     addToVector(index: ChunkIndex, target: vec3 = vec3.create()) {
-        target[0] += index & this.chunk_edge_size;
+        target[0] += index & this.chunk_edge_bitmask;
         index = index >> this.bits_per_chunk_comp;
-        target[1] += index & this.chunk_edge_size;
+        target[1] += index & this.chunk_edge_bitmask;
         index = index >> this.bits_per_chunk_comp;
-        target[2] += index & this.chunk_edge_size;
+        target[2] += index & this.chunk_edge_bitmask;
         return target;
     }
 
@@ -35,8 +36,8 @@ export const ChunkIndex = new class {
     add(index: ChunkIndex, axis: Axis, delta: number) {
         const axis_value = this.getComponent(index, axis) + delta;
         this.register_traversed_chunks = axis_value >> this.bits_per_chunk_comp;
-        return index - index & (this.chunk_edge_size << this.bits_per_chunk_comp * axis_value)  // Removes previous component value
-            + axis_value & this.chunk_edge_size;  // Adds the wrapped component value
+        return index - index & (this.chunk_edge_bitmask << (this.bits_per_chunk_comp * axis_value))  // Removes previous component value
+            + axis_value & this.chunk_edge_bitmask;  // Adds the wrapped component value
     }
 
     addFace(index: ChunkIndex, face: VoxelFace, delta: number = 1) {
@@ -45,14 +46,14 @@ export const ChunkIndex = new class {
 
     // Seeking
     getComponent(index: ChunkIndex, axis: Axis) {
-        return (index >> axis * this.bits_per_chunk_comp) & this.chunk_edge_size;
+        return (index >> (axis * this.bits_per_chunk_comp)) & this.chunk_edge_bitmask;
     }
 
     getChunkEdgeFace(index: ChunkIndex, axis: Axis): VoxelFace | null {
         const comp  = this.getComponent(index, axis);
         if (comp === 0) {
             return FaceUtils.fromParts(axis, -1);
-        } else if (comp === this.chunk_edge_size - 1) {
+        } else if (comp === this.chunk_edge_length - 1) {
             return FaceUtils.fromParts(axis, 1);
         } else {
             return null;
@@ -82,16 +83,16 @@ export const WorldSpaceUtils = new class {
     }
 
     wsGetChunkInner(vec: Readonly<vec3>, target: vec3 = vec3.create()) {
-        target[0] = vec[0] & ChunkIndex.chunk_edge_size;
-        target[1] = vec[1] & ChunkIndex.chunk_edge_size;
-        target[2] = vec[2] & ChunkIndex.chunk_edge_size;
+        target[0] = vec[0] & ChunkIndex.chunk_edge_bitmask;
+        target[1] = vec[1] & ChunkIndex.chunk_edge_bitmask;
+        target[2] = vec[2] & ChunkIndex.chunk_edge_bitmask;
         return target;
     }
 
     wsGetChunkIndex(vec: Readonly<vec3>) {
         return ChunkIndex.fromChunkPosition(
-            vec[0] & ChunkIndex.chunk_edge_size,
-            vec[1] & ChunkIndex.chunk_edge_size,
-            vec[2] & ChunkIndex.chunk_edge_size);
+            vec[0] & ChunkIndex.chunk_edge_bitmask,
+            vec[1] & ChunkIndex.chunk_edge_bitmask,
+            vec[2] & ChunkIndex.chunk_edge_bitmask);
     }
 }();

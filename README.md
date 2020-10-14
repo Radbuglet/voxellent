a6e89cdea5a62b5bdef47e5f21c235ac4c410d3e
Radbuglet <10578060+Radbuglet@users.noreply.github.com>
2020-10-13 20:43:42 -0700

    Fill in "features" and "usage" sections of README.

--- README.md
+++ README.md
@@ -2,15 +2,31 @@
 
 An engine-agnostic library for handling cubic voxel worlds.
 
-Voxellent provides data structures for creating and interacting with infinitely sized cubic worlds and generating optimized meshes to render them. Voxellent makes no assumptions about the underlying rendering technology and the world voxel data structures are completely decoupled from the meshing subsystem.
+Voxellent provides data structures for creating and interacting with infinitely sized cubic worlds and generating optimized meshes to render them. Voxellent makes no assumptions about the underlying rendering technology, and the world voxel data structures are completely decoupled from the meshing subsystem.
 
 ## Features
 
-TODO
+Data submodule:
+
+- `VoxelWorld` and `VoxelChunk`: a data structure for storing an infinitely large grid of chunks.
+- `VoxelPointer`: an object allowing efficient traversal of the chunk grid.
+- `VoxelRayCast`: an object allowing efficient and customizable ray tracing of the voxel world.
+- `VoxelMovableBody`: an object allowing reliable and efficient AABB collision checking against the voxel world.
+- `ChunkLoader`: an object providing efficient rectangular chunk visibility updates.
+- `VoxelChunkData`: a data structure for efficiently storing voxel materials in a chunk.
+
+Meshing submodule:
+
+- `ChunkMeshingQueue`: a manager object for keeping track of chunks in need of a re-meshing.
+- `ChunkMeshing`: a collection of functions providing a highly customizable system for generating optimized chunk meshes.
+- `CoreVertexGeneration`: a collection of functions enabling users to easily generate vertices in a representation-agnostic fashion.
+- `CompactFaceEncoder`: a compression helper to reduce the size of the mesh in VRAM.  
 
 ## Usage
 
-TODO
+Voxellent is available through the NPM package registry and can be installed using `npm install voxellent --save`. This package already contains TypeScript type declaration files—there is no need to install them separately.
+
+You can access the project's documentation [here]() (TODO). 
 
 ## Testing
 
@@ -22,4 +38,4 @@ TODO
 
 ## License
 
-TODO
+[MIT](LICENSE)
---
+++ src/meshing/chunkMeshing.ts
@@ -0,0 +1,52 @@
+import {VoxelChunk} from "..";
+import {P$} from "ts-providers";
+import {ChunkIndex} from "..";
+import {FaceUtils, VoxelFace} from "..";
+import {Rect2} from "..";
+import {VoxelPointer} from "..";
+
+export type VoxelMeshDescriptor<TCtx, TChunk extends P$<typeof VoxelChunk, VoxelChunk<TChunk>>> = {
+    [_ in VoxelFace]: null | { rect: Rect2, generateMesh: (ctx: TCtx, location: VoxelPointer<TChunk>, face: VoxelFace) => void }
+} & {
+    generateInstance?: (ctx: TCtx, location: VoxelPointer<TChunk>) => void
+};
+
+export const ChunkMeshing = new class {
+    generateMesh<TCtx, TChunk extends P$<typeof VoxelChunk, VoxelChunk<TChunk>>>(
+        ctx: TCtx, chunk: TChunk, decodeVoxel: (pointer: VoxelPointer<TChunk>) => VoxelMeshDescriptor<TCtx, TChunk> | null) {
+        const pointer_target = VoxelPointer.empty<TChunk>();
+
+        for (const index of ChunkIndex.iterateAllIndices()) {
+            // Parse voxel
+            pointer_target.setPosInChunk(chunk, index);
+            const own_mesh = decodeVoxel(pointer_target);
+            if (own_mesh == null)
+                continue;
+
+            // Create faces
+            for (const face of FaceUtils.getFaces()) {
+                const own_mesh_face = own_mesh[face];
+                if (own_mesh_face == null) continue;
+
+                // Parse neighbor
+                pointer_target.getNeighborMut(face);
+                const neighbor_mesh = decodeVoxel(pointer_target);
+
+                // Determine whether or not our face should exist
+                if (neighbor_mesh != null) {
+                    const neighbor_occ_face = neighbor_mesh[FaceUtils.getInverse(face)];
+                    if (neighbor_occ_face != null && Rect2.containsRect(neighbor_occ_face.rect, own_mesh_face.rect)) {
+                        continue;  // We can't be visible making this a redundant face.
+                    }
+                }
+
+                // Generate the face!
+                own_mesh_face.generateMesh(ctx, pointer_target, face);
+            }
+
+            // Create instance mesh
+            if (own_mesh.generateInstance != null)
+                own_mesh.generateInstance(ctx, pointer_target);
+        }
+    }
+}();
